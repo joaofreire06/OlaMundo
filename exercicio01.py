4c162d93b3178e19407c0ace9905767f93937ae5
João Gabriel <joao.freiregabriel.2006@gmail.com>
2023-08-01 10:40:36 -0300

    exercicios

---
+++ exercicio01.py
@@ -0,0 +1,11 @@
+#Etapa 1: entrada de dados
+nota1 = float(input("Informe a sua nota 1: "))
+nota2 = float(input("Informe a sua nota 2: "))
+nota3 = float(input("Informe a sua nota 3: "))
+
+#Etapa 2: Processamento de dados
+média = (nota1 + nota2 + nota3) / 3
+
+#Etapa 3: Saída de dados (resposta na tela do usuário)
+print("Sua média é", média)
+
---
+++ exercicio02.py
@@ -0,0 +1,6 @@
+num = int(input("Diga-me um número:"))
+
+ant = num - 1
+suc = num + 1
+
+print("0 antecessor é",ant,"e o sucessor é",suc)
